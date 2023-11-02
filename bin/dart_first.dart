138e8611b30f01de7c46842226a4b699daa206ba
Shuvon-majhi <shuvon.majhi@gmail.com>
2023-11-02 12:49:29 +0600

    updated

--- bin/dart_first.dart
+++ bin/dart_first.dart
@@ -9,6 +9,9 @@ class Shuvon{
   static majhi(){
     int? num=int.parse(stdin.readLineSync()!);
     int? number=int.parse(stdin.readLineSync()!);
+    int? number1=int.parse(stdin.readLineSync()!);
+    int? a;
+    int? b;
     for(int i=1;i<=10;i++){
       print('$num * $i = ${i*num} ');
     }
