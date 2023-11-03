3b975719656f68a95f7a1df01c8d26ae1c19d61b
Shuvon-majhi <shuvon.majhi@gmail.com>
2023-11-03 11:13:22 +0600

    updated

--- bin/dart_first.dart
+++ bin/dart_first.dart
@@ -1,14 +1,46 @@
 import 'dart:io';
 void main(){
-  Shuvon.majhi();
-}
+  List<String> Universities=['buet','ruet','cuet','kuet','du'];
 
-class Shuvon{
-  static majhi(){
-    int? num=int.parse(stdin.readLineSync()!);
-    for(int i=1;i<=10;i++){
-      print('$num * $i = ${i*num} ');
-    }
-  }
+  print(Universities.first);
+  print(Universities.last);
+  print(Universities.length);
+  print(Universities);
+  print(Universities.elementAt(2));
+  print(Universities[2]);
+
+
+  Map<int,String> students= {
+    5: 'alex',
+    3: 'roy',
+    4: 'joy',
+    2: 'tom',
+    1: 'cook',
+  };
+
+  print(students);
+  print(students.length);
+  print(students.keys);
+  print(students.values);
+  print(students.entries);
+
+
+  Map<int, Map<String, String>> studentHistory = {
+    1: {
+      'name': 'Siam',
+      'fatherName': 'skdfjd',
+      'mothersName': 'skdfjd',
+    },
+    2: {
+      'name': 'Satil',
+      'fatherName': 'skdfjd',
+      'mothersName': 'skdfjd',
+    },
+  };
+
+
+  print(studentHistory[2]);
+
+  print(studentHistory.entries);
 }
 
